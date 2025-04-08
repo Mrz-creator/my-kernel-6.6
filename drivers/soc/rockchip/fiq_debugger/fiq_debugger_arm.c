5dd7ee3b52414b42cd1124e5f0a93ea2e6544429
hao.zhang <hao.zhang@coolkit.cn>
2025-04-08 20:06:17 +0800

    fix build bool type

--- drivers/soc/rockchip/fiq_debugger/fiq_debugger_arm.c
+++ drivers/soc/rockchip/fiq_debugger/fiq_debugger_arm.c
@@ -183,7 +183,7 @@ struct stacktrace_state {
 	unsigned int depth;
 };
 
-static int report_trace(struct stackframe *frame, void *d)
+static bool report_trace(struct stackframe *frame, void *d)
 {
 	struct stacktrace_state *sts = d;
 
@@ -193,7 +193,7 @@ static int report_trace(struct stackframe *frame, void *d)
 			frame->pc, frame->pc, frame->lr, frame->lr,
 			frame->sp, frame->fp);
 		sts->depth--;
-		return 0;
+		return false;
 	}
 	sts->output->printf(sts->output, "  ...\n");
 
@@ -263,7 +263,7 @@ void fiq_debugger_dump_stacktrace(struct fiq_debugger_output *output,
 		frame.lr = regs->ARM_lr;
 		frame.pc = regs->ARM_pc;
 		output->printf(output, "\n");
-		walk_stackframe(&frame, (bool)report_trace, &sts);
+		walk_stackframe(&frame, report_trace, &sts);
 		return;
 	}
 
