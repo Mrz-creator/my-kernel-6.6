632cf4448e588cc7a13b7afed2b3f114280a3812
hao.zhang <hao.zhang@coolkit.cn>
2025-04-09 11:21:13 +0800

    fix build modify kernel/rcu/tree_stall.h

--- kernel/rcu/tree_stall.h
+++ kernel/rcu/tree_stall.h
@@ -16,6 +16,7 @@
 /* panic() on RCU Stall sysctl. */
 int sysctl_panic_on_rcu_stall __read_mostly;
 int sysctl_max_rcu_stall_to_panic __read_mostly;
+ATOMIC_NOTIFIER_HEAD(rcu_stall_notifier_list);
 
 #ifdef CONFIG_PROVE_RCU
 #define RCU_STALL_DELAY_DELTA		(5 * HZ)
@@ -654,6 +655,8 @@ static void print_other_cpu_stall(unsigned long gp_seq, unsigned long gps)
 
 	rcu_check_gp_kthread_expired_fqs_timer();
 	rcu_check_gp_kthread_starvation();
+
+	atomic_notifier_call_chain(&rcu_stall_notifier_list, 0, NULL);
 
 	panic_on_rcu_stall();
 
